21c23c645c6810111b32d642a1ceed92aedfb149
Arfst Ludwig <mail@arfst.de>
2024-07-27 17:56:37 +0200

    tunnel discrete curvature

--- tunnel.c
+++ tunnel.c
@@ -21,6 +21,7 @@
 #define COLOR_PERIOD (2 * COLOR_LENGTH)
 #define TUNNEL_RADIUS 1000
 #define FOCAL_LENGTH 500
+#define OFFSET_SCALE 324
 
 #define FRAMES 10
 #define DELAY 60
@@ -99,17 +100,48 @@ int main(int ac, char *av[])
   init();
   int d0 = TUNNEL_RADIUS * FOCAL_LENGTH / CENTER_RADIUS - FOCAL_LENGTH;
 
+  static int angle_x[PERIODS * FRAMES];
+  static int offset_x[PERIODS * FRAMES];
+  static int angle_y[PERIODS * FRAMES];
+  static int offset_y[PERIODS * FRAMES];
+
   display_create(display_select);
 
   for (int frame = 0; !display_button(); frame = (frame + 1) % FRAMES)
   {
-    //display_clear();
+    if (!frame)
+    {
+      // TODO
+      static int curvature_x[PERIODS * FRAMES];
+      for (int i = 0; i < PERIODS * FRAMES; i++)
+	curvature_x[i] = 100;
 
-    int offset = frame * COLOR_PERIOD / FRAMES;
+      static int curvature_y[PERIODS * FRAMES];
+      for (int i = 0; i < PERIODS * FRAMES; i++)
+	curvature_y[i] = 50;
+
+
+      // integrate
+      angle_x[0] = 0;
+      for (int i = 1; i < PERIODS * FRAMES; i++)
+	angle_x[i] = angle_x[i - 1] + curvature_x[i - 1];
+      offset_x[0] = 0;
+      for (int i = 1; i < PERIODS * FRAMES; i++)
+	offset_x[i] = offset_x[i - 1] + angle_x[i - 1];
+
+      angle_y[0] = 0;
+      for (int i = 1; i < PERIODS * FRAMES; i++)
+	angle_y[i] = angle_y[i - 1] + curvature_y[i - 1];
+      offset_y[0] = 0;
+      for (int i = 1; i < PERIODS * FRAMES; i++)
+	offset_y[i] = offset_y[i - 1] + angle_y[i - 1];
+    }
+
+    int offset_z = frame * COLOR_PERIOD / FRAMES;
 
     for (int i = PERIODS - 1; i >= 0; i--)
     {
-      int d1 = d0 + i * COLOR_PERIOD - offset;
+      int d1 = d0 + i * COLOR_PERIOD - offset_z;
       int d2 = d1 + COLOR_LENGTH;
       int d3 = d2 + COLOR_LENGTH;
       if (d1 < d0)
@@ -122,20 +154,22 @@ int main(int ac, char *av[])
       int r2 = (TUNNEL_RADIUS - d2 / 35) * FOCAL_LENGTH / (d2 + FOCAL_LENGTH);
       int r3 = (TUNNEL_RADIUS - d3 / 35) * FOCAL_LENGTH / (d3 + FOCAL_LENGTH);
 
-      int curve_x = 0;
-      int curve_y = 0;
+      int i1 = i * FRAMES - frame;
+      if (i1 < 0)
+	i1 = 0;
+      int i2 = i * FRAMES - frame + FRAMES / 2;
+      if (i2 < 0)
+	i2 = 0;
 
-      int xx1 = curve_x * WIDTH * (d1 - d0) / (PERIODS * COLOR_PERIOD)
-        * (d1 - d0) / (PERIODS * COLOR_PERIOD) / 15;
-      int xx2 = curve_x * WIDTH * (d2 - d0) / (PERIODS * COLOR_PERIOD)
-        * (d2 - d0) / (PERIODS * COLOR_PERIOD) / 15;
+      int xx1 = offset_x[i1] / OFFSET_SCALE;
+      int xx2 = offset_x[i2] / OFFSET_SCALE;
+
       int x1 = xx1 * FOCAL_LENGTH / (d1 + FOCAL_LENGTH);
       int x2 = xx2 * FOCAL_LENGTH / (d2 + FOCAL_LENGTH);
 
-      int yy1 = curve_y * WIDTH * (d1 - d0) / (PERIODS * COLOR_PERIOD)
-        * (d1 - d0) / (PERIODS * COLOR_PERIOD) / 15;
-      int yy2 = curve_y * WIDTH * (d2 - d0) / (PERIODS * COLOR_PERIOD)
-        * (d2 - d0) / (PERIODS * COLOR_PERIOD) / 15;
+      int yy1 = offset_y[i1] / OFFSET_SCALE;
+      int yy2 = offset_y[i2] / OFFSET_SCALE;
+
       int y1 = yy1 * FOCAL_LENGTH / (d1 + FOCAL_LENGTH);
       int y2 = yy2 * FOCAL_LENGTH / (d2 + FOCAL_LENGTH);
 
